8e9437b4bb12f3837e64d055442061fc250f850b
Emmanuel Marchal <81331382+irbt@users.noreply.github.com>
2022-11-30 11:34:44 +0100

    String in reverse order (recursion)

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿//Given a string, write a method that prints it in reverse order.
+
+
+
+namespace Program{
+    class StringInReverseOrderRecursionTask{
+        public static void Main(string [] args){
+            Console.Clear();
+            static long StringInReverseOrderRecursion(string s){
+                
+                 return  ;
+            }
+            StringInReverseOrderRecursion("abcde");// → "edcba"
+            StringInReverseOrderRecursion("Sed lectus est, elementum ut urna eu");// → "ue anru tu mutnemele ,tse sutcel deS"
+        }
+    }
+}
